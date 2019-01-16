180c3c29feec7c88d9c78a5f3eb0c5bfccf81877
Curso <Curso@Formacion-TOS14.ipartekaula.com>
2019-01-16 14:16:34 +0100

    bd renovada con procedimientos

--- SQL/dgt.sql
+++ SQL/dgt.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `dgt` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `dgt`;
 -- MySQL dump 10.13  Distrib 8.0.13, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: dgt
@@ -294,4 +296,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-16 14:09:46
+-- Dump completed on 2019-01-16 14:14:56
